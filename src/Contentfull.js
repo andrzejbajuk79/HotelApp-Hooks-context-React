9653533df6cabd90e2ef746760adccce34f30804
andrzejbajuk79 <ptasiek3003@gmail.com>
2020-04-30 13:45:20 +0200

    test

--- src/Contentfull.js
+++ src/Contentfull.js
@@ -4,3 +4,10 @@ export default createClient({
  space: '1x5qwg7ddf3f',
  accessToken: 'H7Wv3eAmUumlIeaxAKQwLrl2zoWD3ahKtylHUfP3VMU',
 });
+
+// import {createClient} from 'contentful';
+
+// export default createClient({
+//  space: process.env.REACT_APP_API_SPACE,
+//  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+// });
